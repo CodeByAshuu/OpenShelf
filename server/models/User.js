be20cb2561c967302bae180fd881541798354676
Sagar <sagarsahu5976@gmail.com>
2025-12-28 17:21:22 +0530

    Updated User Controllers,route,model/ Created Profile

--- server/models/User.js
+++ server/models/User.js
@@ -1,14 +1,17 @@
 const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
-  name: { type: String, required: true },
+  name: { type: String, required: true, trim: true },
   email: {
     type: String,
     required: true,
     unique: true,
     match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, 'Please fill a valid email address']
   },
-  password: { type: String, required: true, minlength: 6 }
+  password: { type: String, required: true, minlength: 6 },
+  bio: { type: String, default: "" },
+  profileImage: { type: String, default: "" },
+  createdAt: { type: Date, default: Date.now }
 });
 
 module.exports = mongoose.model('User', userSchema);
